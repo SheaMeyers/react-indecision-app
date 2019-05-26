a9dfd9187a478183465ebc41930612f659a828d7
SheaMeyers <shea.meyers@gmail.com>
2019-05-26 12:42:08 +0200

    Install babel

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,13 @@
+'use strict';
+
 console.log('App.js is running');
 
 // JSX - JavaScript XML
-//var template = <p>This is JSX from app.js</p>;
-
-var template = React.createElement("h1", {
-    id: "someId"
-  }, "Something new");
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
 
 var appRoot = document.getElementById('app');
 
