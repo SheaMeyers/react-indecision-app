feb89c7cbca28a5683c3878a9a1ac3f33aba613d
SheaMeyers <shea.meyers@gmail.com>
2019-08-21 19:07:36 +0200

    Use ES6 imports

--- src/app.js
+++ src/app.js
@@ -1,1 +1,18 @@
-console.log('app.js is running!');
+//import './utils';
+// import { add, square } from './utils.js';
+
+// console.log('app.js is running!');
+// console.log(square(4));
+// console.log(add(100, 23));
+
+// create person.js file
+// export named export, isAdult(18) true if adult, otherwise false
+// export named export, canDrink(19) true if 19 and over, otherwise false
+
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(17));
+console.log(isAdult(18));
+console.log(canDrink(18));
+console.log(canDrink(19));
