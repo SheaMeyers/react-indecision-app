bddfec633a8aa257a2d6cf60f16ac6f567a30759
SheaMeyers <shea.meyers@gmail.com>
2019-08-26 21:33:52 +0200

    Use and import npm modules

--- src/app.js
+++ src/app.js
@@ -1,22 +1,6 @@
-//import './utils';
-// import subtract, { add, square } from './utils.js';
+// install -> import -> use
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-// console.log('app.js is running!');
-// console.log(square(4));
-// console.log(add(100, 23));
-// console.log(subtract(100, 81));
-
-// create person.js file
-// export named export, isAdult(18) true if adult, otherwise false
-// export named export, canDrink(19) true if 19 and over, otherwise false
-
-
-import isSenior, { isAdult, canDrink } from './person.js';
-
-console.log(isAdult(17));
-console.log(isAdult(18));
-console.log(canDrink(18));
-console.log(canDrink(19));
-
-console.log(isSenior(64));
-console.log(isSenior(65));
+const template = React.createElement('p', {}, 'testing 123');
+ReactDOM.render(template, document.getElementById('app'));
